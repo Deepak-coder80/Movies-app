f67a61c0931168367d6338b4b4cbf1da779821f9
Deepak M S <souparnikad40@gmail.com>
2021-08-28 19:55:45 +0530

    Project Started

---
+++ android/app/src/main/kotlin/com/example/deepak/app/movie_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.deepak.app.movie_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieListView extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movie List"),
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+    );
+  }
+}
